6fa30514986328ee526e7b7303bcabab8819cbd0
moraeal <moraeal@gmail.com>
2026-09-01 17:28:56 +0900

    칼럼 영문 번역 인덱스 추가

---
+++ src/data/contentTranslations/columns/index.ts
@@ -0,0 +1,16 @@
+import { issue01 } from "./issue01";
+import { issue02 } from "./issue02";
+import { issue03 } from "./issue03";
+import { issue04 } from "./issue04";
+import { issue05 } from "./issue05";
+import { issue06 } from "./issue06";
+import type { ColumnTranslation } from "../types";
+
+export const columnTranslations: Record<number, ColumnTranslation> = {
+  1: issue01,
+  2: issue02,
+  3: issue03,
+  4: issue04,
+  5: issue05,
+  6: issue06,
+};
