0818ece060a3282ee876c479734b5409c3dd5326
moraeal <moraeal@gmail.com>
2026-09-07 08:49:13 -0700

    Remove obsolete content registry index

--- src/content/columns/index.ts
+++
@@ -1,6 +0,0 @@
-import type { SeedColumn } from "../../data/columns";
-import { lhReformPolitics2026Column } from "./lh-reform-politics-2026";
-
-export const contentColumns: SeedColumn[] = [
-  lhReformPolitics2026Column,
-];
