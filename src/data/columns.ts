ee31ac685a08f140fec1214e1ce9195d602144c4
moraeal <moraeal@gmail.com>
2026-09-07 07:40:10 -0700

    Use content registry for columns

--- src/data/columns.ts
+++ src/data/columns.ts
@@ -1,10 +1,13 @@
-export type { SeedColumn } from "./columnsLegacy";
-export * from "./columnsLegacy";
+import { contentColumns } from "../content/columns";
 import { columns as legacyColumns } from "./columnsLegacy";
 import type { SeedColumn } from "./columnsLegacy";
-import { lhReformColumn } from "./columns/lhReformColumn";
 
-export const columns: SeedColumn[] = [lhReformColumn, ...legacyColumns];
+export type { SeedColumn } from "./columnsLegacy";
+
+export const columns: SeedColumn[] = [...contentColumns, ...legacyColumns];
+
 export const getColumn = (slug: string) => columns.find((column) => column.slug === slug);
-export const getColumnsNewestFirst = () => [...columns].sort((a, b) => b.issue - a.issue);
+
+export const getColumnsNewestFirst = () => [...columns].sort((a, b) => b.date.localeCompare(a.date) || b.issue - a.issue);
+
 export const getLatestColumn = () => getColumnsNewestFirst()[0];
