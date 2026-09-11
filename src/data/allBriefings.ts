cc83edb15259b7305b9813fb61bd08ebfe61c201
moraeal <moraeal@gmail.com>
2026-09-10 17:10:27 -0700

    List civic briefing 09 on social economy fairness

--- src/data/allBriefings.ts
+++ src/data/allBriefings.ts
@@ -4,8 +4,9 @@ import { nationalBudgetBriefing } from "./nationalBudgetBriefing";
 import { publicBroadcastingBriefing } from "./publicBroadcastingBriefing";
 import { publicInterestTravelBriefing } from "./publicInterestTravelBriefing";
 import { socialEconomyBriefing } from "./socialEconomyBriefing";
+import { socialEconomyFairnessBriefing } from "./socialEconomyFairnessBriefing";
 
-const allBriefings: Briefing[] = [socialEconomyBriefing, publicInterestTravelBriefing, publicBroadcastingBriefing, nationalBudgetBriefing, gyeonggiBriefingDisplay, ...briefings];
+const allBriefings: Briefing[] = [socialEconomyFairnessBriefing, socialEconomyBriefing, publicInterestTravelBriefing, publicBroadcastingBriefing, nationalBudgetBriefing, gyeonggiBriefingDisplay, ...briefings];
 
 export const getAllBriefingsNewestFirst = () => [...allBriefings].sort((a, b) => {
   const dateOrder = b.date.localeCompare(a.date);
