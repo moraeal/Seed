9e4f85c4c7bba485571bcb43e58bd37fb24c6cb7
moraeal <moraeal@gmail.com>
2026-09-11 23:46:20 -0700

    Remove freedom-specific detail route

--- src/pages/SeedLanguageDetail.tsx
+++ src/pages/SeedLanguageDetail.tsx
@@ -1,9 +1,5 @@
-import { useParams } from "react-router-dom";
-import FreedomSeedLanguageDetail from "./FreedomSeedLanguageDetail";
 import SeedLanguageDetailBase from "./SeedLanguageDetailBase";
 
 export default function SeedLanguageDetail() {
-  const { slug = "" } = useParams();
-  if (slug === "freedom-as-citizen-agency") return <FreedomSeedLanguageDetail />;
   return <SeedLanguageDetailBase />;
 }
