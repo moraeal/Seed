f39bd6584dd8e22f018603be53fe650db8a5907d
moraeal <moraeal@gmail.com>
2026-09-11 22:57:40 -0700

    Feature freedom article on main

--- src/data/columns.ts
+++ src/data/columns.ts
@@ -1,3 +1,4 @@
+import { freedomIsSubjecthoodColumn } from "./columns/freedom-is-subjecthood";
 import { stateCannotMonopolizeLifeColumn } from "./columns/state-cannot-monopolize-life";
 import { civicGroupsAreNotStateVanguardColumn } from "./columns/civic-groups-are-not-state-vanguard";
 import { tenPercentPowerColumn } from "./columns/tenPercentPowerColumn";
@@ -8,7 +9,7 @@ import type { SeedColumn } from "./columnsLegacy";
 
 export type { SeedColumn } from "./columnsLegacy";
 
-export const columns: SeedColumn[] = [stateCannotMonopolizeLifeColumn, civicGroupsAreNotStateVanguardColumn, tenPercentPowerColumn, prosecutionReformPowerTransferColumn, lhReformColumn, ...legacyColumns];
+export const columns: SeedColumn[] = [freedomIsSubjecthoodColumn, stateCannotMonopolizeLifeColumn, civicGroupsAreNotStateVanguardColumn, tenPercentPowerColumn, prosecutionReformPowerTransferColumn, lhReformColumn, ...legacyColumns];
 
 export const getColumn = (slug: string) => columns.find((column) => column.slug === slug);
 
