5d9d050f79286d0b73390fb727b6606c8fff7288
moraeal <moraeal@gmail.com>
2026-09-16 19:27:00 -0700

    Prepare civic briefing on party dissolution

--- src/data/contentTranslations/briefings.ts
+++ src/data/contentTranslations/briefings.ts
@@ -2,9 +2,11 @@ import type { BriefingTranslation } from "./types";
 import { briefingTranslations as legacyBriefingTranslations } from "./briefingsLegacy";
 import { socialEconomyFairnessTranslation } from "./briefing09";
 import { hearingAccountabilityTranslation } from "./briefing10";
+import { partyDissolutionTranslation } from "./briefing11";
 import { yeosuIslandExpoBriefingTranslation } from "./briefingYeosuIslandExpo";
 
 export const briefingTranslations: Record<string, BriefingTranslation> = {
+  "can-half-the-nation-be-dissolved": partyDissolutionTranslation,
   "yeosu-world-island-expo": yeosuIslandExpoBriefingTranslation,
   "confirmation-hearings-zero-witnesses": hearingAccountabilityTranslation,
   "social-economy-fair-competition": socialEconomyFairnessTranslation,
