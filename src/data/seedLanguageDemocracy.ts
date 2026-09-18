0fb5a5be05b03486a4b8ccd8d86cde385a6e44fc
moraeal <moraeal@gmail.com>
2026-09-18 07:26:02 -0700

    Add homeHeroEligible property to democracyDeepKo

--- src/data/seedLanguageDemocracy.ts
+++ src/data/seedLanguageDemocracy.ts
@@ -159,6 +159,7 @@ const democracyFeatureEn: SeedLanguageArticle = {
 const democracyDeepKo: SeedLanguageArticle = {
   slug: "democracy-citizens-deep-read",
   newsletterEligible: false,
+  homeHeroEligible: false,
   term: "민주 · 깊게 읽기",
   date: "2026-09-09",
   readMinutes: 15,
