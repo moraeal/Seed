519614faeb89756164626a8fee90e1a86f24317d
moraeal <moraeal@gmail.com>
2026-09-17 19:55:51 -0700

    Expose complete monitoring registry for SEO and previews

--- src/data/newsTrackerRegistry.ts
+++ src/data/newsTrackerRegistry.ts
@@ -1,8 +1,14 @@
 import {
   getPublicInterestWatchCase as getBasePublicInterestWatchCase,
   newsTrackerCases as baseNewsTrackerCases,
+  publicInterestWatchCases as basePublicInterestWatchCases,
 } from "./publicInterestWatch";
 import { olympicParkElectionProtestTracker } from "./olympicParkElectionProtestTracker";
+
+export const publicInterestWatchCases = [
+  olympicParkElectionProtestTracker,
+  ...basePublicInterestWatchCases,
+];
 
 export const newsTrackerCases = [
   olympicParkElectionProtestTracker,
