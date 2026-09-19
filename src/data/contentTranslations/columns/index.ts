dd89b274e846a18203de8c730851f068f9195fa4
moraeal <moraeal@gmail.com>
2026-09-18 20:44:23 -0700

    Add issue24 to columnTranslations

--- src/data/contentTranslations/columns/index.ts
+++ src/data/contentTranslations/columns/index.ts
@@ -21,6 +21,7 @@ import { issue20 } from "./issue20";
 import { issue21 } from "./issue21";
 import { issue22 } from "./issue22";
 import { issue23 } from "./issue23";
+import { issue24 } from "./issue24";
 import type { ColumnTranslation } from "../types";
 
 export const columnTranslations: Record<number, ColumnTranslation> = {
@@ -47,4 +48,5 @@ export const columnTranslations: Record<number, ColumnTranslation> = {
   21: issue21,
   22: issue22,
   23: issue23,
+  24: issue24,
 };
