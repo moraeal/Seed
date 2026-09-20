37f9947ea38db89107a5d7e105d385c73ad3013f
moraeal <moraeal@gmail.com>
2026-09-20 16:34:12 -0700

    공공의료는 병원 간판으로 증명되지 않는다
    씨앗의 소리 단독 칼럼을 한국어·영어판과 시각자료로 게시합니다.

--- src/data/columns.ts
+++ src/data/columns.ts
@@ -1,3 +1,4 @@
+import { publicHealthFunctionNetworkColumn } from "./columns/publicHealthFunctionNetworkColumn";
 import { korea97GenerationColumn } from "./columns/korea97GenerationColumn";
 import { wealthCrossesBordersColumn } from "./columns/wealthCrossesBordersColumn";
 import { militaryAcademyIntegrationColumn } from "./columns/militaryAcademyIntegrationColumn";
@@ -16,7 +17,7 @@ import type { SeedColumn } from "./columnsLegacy";
 
 export type { SeedColumn } from "./columnsLegacy";
 
-export const columns: SeedColumn[] = [korea97GenerationColumn, wealthCrossesBordersColumn, fukushimaJourneyColumn, militaryAcademyIntegrationColumn, nuclearPolicyReversalColumn, farmlandOwnershipPressureColumn, silenceAndPowerColumn, citizenizationBeforeAdvancementColumn, stateCannotMonopolizeLifeColumn, civicGroupsAreNotStateVanguardColumn, tenPercentPowerColumn, prosecutionReformPowerTransferColumn, lhReformColumn, ...legacyColumns];
+export const columns: SeedColumn[] = [publicHealthFunctionNetworkColumn, korea97GenerationColumn, wealthCrossesBordersColumn, fukushimaJourneyColumn, militaryAcademyIntegrationColumn, nuclearPolicyReversalColumn, farmlandOwnershipPressureColumn, silenceAndPowerColumn, citizenizationBeforeAdvancementColumn, stateCannotMonopolizeLifeColumn, civicGroupsAreNotStateVanguardColumn, tenPercentPowerColumn, prosecutionReformPowerTransferColumn, lhReformColumn, ...legacyColumns];
 
 export const hotIssueColumnSlugs = new Set([
   "democratic-party-nuclear-policy-reversal",
